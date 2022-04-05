3032a8236e1cac51a77885b344b65fe9322ab255
Sarahqtw <Sarahqtw@hotmail.com>
2022-04-05 16:25:41 +0800

    Added sliders for price and wait time

---
+++ lunch-go-where/src/components/AutocompleteLocation.js
@@ -0,0 +1,23 @@
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export default function ComboBox() {
+	return (
+		<Autocomplete
+			disablePortal
+			id="combo-box-demo"
+			options={locations}
+			sx={{ width: 300 }}
+			renderInput={(params) => <TextField {...params} label="Locations" />}
+		/>
+	);
+}
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const locations = [
+	'Amoy Street Food Centre',
+	'Hong Lim Market & Food Centre',
+	'Tanjong Pagar Plaza Market & Food Centre',
+	'Maxwell Food Centre',
+	"People's Park Food Centre"
+];
--- lunch-go-where/src/pages/Home.js
+++ lunch-go-where/src/pages/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Box, Button } from '@mui/material';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import AutocompleteLocation from '../components/AutocompleteLocation';
+import Slider from '../components/Slider';
 const Home = () => {
 	return (
 		<React.Fragment>
 			<ResponsiveAppBar />
+			<Typography variant="h4">SEARCH</Typography>
+			<Box>
+				<form>
+					<AutocompleteLocation />
+					<Slider label={'Price Range'} step={5} defaultValue={5} min={0} max={20} />
+					<Slider label={'Wait Time'} step={5} defaultValue={5} min={0} max={30} />
+					<Button color="secondary" type="submit" variant="contained">
+						SEARCH
+					</Button>
+				</form>
+			</Box>
 		</React.Fragment>
 	);
 };
