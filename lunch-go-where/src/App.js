6c8b16910a6787cccd607d31dd14240f7f9105e7
Sarahqtw <Sarahqtw@hotmail.com>
2022-04-05 15:59:53 +0800

    Added a home page with a nav bar

--- lunch-go-where/src/App.js
+++ lunch-go-where/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
-
+import Home from './pages/Home';
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Landing />} />
+			<Route path="/home" element={<Home />} />
 		</Routes>
 	);
 }
---
+++ lunch-go-where/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import ResponsiveAppBar from '../components/ResponsiveAppBar';
+const Home = () => {
+	return (
+		<React.Fragment>
+			<ResponsiveAppBar />
+		</React.Fragment>
+	);
+};
+
+export default Home;
