3f6981765f9b701bccb9e9fbaf7161bcbb6d550e
Sarahqtw <Sarahqtw@hotmail.com>
2022-04-06 14:29:06 +0800

    fixed bug with login api call on homepage

--- lunch-go-where/src/utils/apiCalls.js
+++ lunch-go-where/src/utils/apiCalls.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 const baseURL = 'http://localhost:5001';
 
+const login = async (body) => {
+	await axios.post(baseURL + '/login', body);
+};
+
 const postNewStall = async (body) => {
 	await axios.post(baseURL + '/new', body);
 };
@@ -9,6 +13,6 @@ const getRecommendedStalls = async () => {
 	const response = await axios.get(baseURL + '/home');
 	return response.data;
 };
-const apiCalls = { postNewStall, getRecommendedStalls };
+const apiCalls = { postNewStall, getRecommendedStalls, login };
 
 export default apiCalls;
