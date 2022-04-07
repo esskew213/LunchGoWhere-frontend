f1fdba7c91a34baf913a9d4b50369271ed844d9c
0bserver <rlee767@gmail.com>
2022-04-07 16:31:35 +0800

    retrieved location

--- lunch-go-where/src/components/NewStallForm.js
+++ lunch-go-where/src/components/NewStallForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useSetInputState from "../hooks/useSetInputState";
 import {
@@ -30,6 +30,33 @@ const NewStallForm = () => {
   const handleLocationChange = (evt, value) => {
     setLocation(value);
   };
+
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+    console.log(lat, lng);
+  });
 
   let navigate = useNavigate();
   let path = "../review";
