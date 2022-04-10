7be3904d024c165c63aee9df7e029ac7f735e774
Lau Yu Yao <lauyuyao@Laus-MacBook-Pro.local>
2022-04-10 14:21:42 +0800

    MInor updates on comments
    regarding black border and setting username back to empty

--- lunch-go-where/src/pages/Landing.js
+++ lunch-go-where/src/pages/Landing.js
@@ -23,7 +23,6 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
   textAlign: "center",
@@ -71,6 +70,7 @@ const Landing = () => {
     console.log("SUBMITTING", username, password);
     try {
       await apis.login({ username: username, password: password });
+      setUsername("");
       setPassword("");
       navigate("/home");
     } catch (err) {
