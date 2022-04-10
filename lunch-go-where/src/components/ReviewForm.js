7f76ad38ceb308488d707aa22876697991a153bd
Sarahqtw <Sarahqtw@hotmail.com>
2022-04-10 18:50:49 +0800

    fixed bug with switches in review form
    need to implement axios put call, and auto refresh of page on review submission

--- lunch-go-where/src/components/ReviewForm.js
+++ lunch-go-where/src/components/ReviewForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useSetInputState from '../hooks/useSetInputState';
 import { TextField, FormControl, Button, Box, Checkbox, FormControlLabel, FormGroup, Switch } from '@mui/material';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
@@ -8,24 +8,43 @@ import Favorite from '@mui/icons-material/Favorite';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import apis from '../utils/apiCalls';
 const ReviewForm = ({ stallID }) => {
+	const [ hasReviewed, setHasReviewed ] = useState(false);
 	const [ price, setPrice, handlePriceChange, resetPrice ] = useSetInputState(0);
 	const [ waitTime, setWaitTime, handleWaitTimeChange, resetWaitTime ] = useSetInputState(5);
-	const [ wouldEatAgain, setWouldEat, handleWouldEatChange, resetWouldEat ] = useSetInputState(false);
-	const [ wouldQueueAgain, setWouldQueue, handleWouldQueueChange, resetWouldQueue ] = useSetInputState(false);
+	const [ wouldEatAgain, setWouldEat ] = useSetInputState(false);
+	const [ wouldQueueAgain, setWouldQueue ] = useSetInputState(false);
+	const handleWouldEatChange = (evt) => {
+		setWouldEat((prevState) => !prevState);
+		console.log(wouldEatAgain);
+	};
+	const handleWouldQueueChange = (evt) => {
+		setWouldQueue((prevState) => !prevState);
+		console.log(wouldQueueAgain);
+	};
 	const handleSubmit = (evt) => {
 		evt.preventDefault();
 		console.log(price, waitTime, wouldEatAgain, wouldQueueAgain, stallID);
 		apis.postNewReview({ price, waitTime, wouldEatAgain, wouldQueueAgain, stallID });
+		resetPrice();
+		resetWaitTime();
+		setWouldEat(false);
+		setWouldQueue(false);
 	};
 	useEffect(() => {
 		apis
 			.getExistingReview(stallID)
 			.then((res) => {
-				const { price, waitTime, wouldEatAgain, wouldQueueAgain } = res.data;
-				setPrice(price);
-				setWaitTime(waitTime);
-				setWouldEat(wouldEatAgain);
-				setWouldQueue(wouldQueueAgain);
+				if (res.data.review !== null) {
+					console.log('Previous review detected.', res.data);
+					const { price, waitTime, wouldEatAgain, wouldQueueAgain } = res.data.review;
+					setPrice(price);
+					setWaitTime(waitTime);
+					setWouldEat(wouldEatAgain);
+					setWouldQueue(wouldQueueAgain);
+					setHasReviewed(true);
+				} else {
+					console.log('No previous review detected.');
+				}
 			})
 			.catch((err) => {
 				console.log(err.response);
