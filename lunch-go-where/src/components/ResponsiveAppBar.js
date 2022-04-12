9ce82a48a504e9be786154ca8d37f98b01f2a9eb
Sarahqtw <Sarahqtw@hotmail.com>
2022-04-12 20:45:11 +0800

    implemented redirect from homepage if logged in

--- lunch-go-where/src/components/ResponsiveAppBar.js
+++ lunch-go-where/src/components/ResponsiveAppBar.js
@@ -15,138 +15,126 @@ import apis from "../utils/apiCalls";
 import { useNavigate } from "react-router-dom";
 
 const pages = {
-    home: "Home",
-    new: "Add A Stall",
-    favourites: "My Favourites",
+	home: "Home",
+	new: "Add A Stall"
 };
 
 const logo = "LUNCHGOWHERE";
 const ResponsiveAppBar = () => {
-    const navigate = useNavigate();
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const navigate = useNavigate();
+	const [ anchorElNav, setAnchorElNav ] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleLogout = async () => {
-        const logout = await apis.logoutUser();
-        navigate("/");
-    };
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleLogout = async () => {
+		const logout = await apis.logoutUser();
+		navigate("/");
+	};
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
 
-    return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-                    >
-                        {logo}
-                    </Typography>
+	return (
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
+						{logo}
+					</Typography>
 
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "flex", md: "none" },
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: "block", md: "none" },
-                            }}
-                        >
-                            {Object.keys(pages).map((page) => (
-                                <MenuItem
-                                    key={page}
-                                    onClick={handleCloseNavMenu}
-                                >
-                                    <Link
-                                        style={{
-                                            textDecoration: "none",
-                                            color: "black",
-                                        }}
-                                        to={`/${page}`}
-                                    >
-                                        {pages[page]}
-                                    </Link>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "flex", md: "none" },
-                        }}
-                    >
-                        {logo}
-                    </Typography>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex" },
-                        }}
-                    >
-                        {Object.keys(pages).map((page) => (
-                            <Button
-                                key={page}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: "white", display: "block" }}
-                            >
-                                <Link
-                                    style={{
-                                        textDecoration: "none",
-                                        color: "white",
-                                    }}
-                                    to={`/${page}`}
-                                >
-                                    {pages[page]}
-                                </Link>
-                            </Button>
-                        ))}
-                    </Box>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: "flex", md: "none" }
+						}}
+					>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit"
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left"
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left"
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" }
+							}}
+						>
+							{Object.keys(pages).map((page) => (
+								<MenuItem key={page} onClick={handleCloseNavMenu}>
+									<Link
+										style={{
+											textDecoration: "none",
+											color: "black"
+										}}
+										to={`/${page}`}
+									>
+										{pages[page]}
+									</Link>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{
+							flexGrow: 1,
+							display: { xs: "flex", md: "none" }
+						}}
+					>
+						{logo}
+					</Typography>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: "none", md: "flex" }
+						}}
+					>
+						{Object.keys(pages).map((page) => (
+							<Button
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: "white", display: "block" }}
+							>
+								<Link
+									style={{
+										textDecoration: "none",
+										color: "white"
+									}}
+									to={`/${page}`}
+								>
+									{pages[page]}
+								</Link>
+							</Button>
+						))}
+					</Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <LogoutIcon
-                            onClick={handleLogout}
-                            sx={{ p: 0 }}
-                        ></LogoutIcon>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+					<Box sx={{ flexGrow: 0 }}>
+						<LogoutIcon onClick={handleLogout} sx={{ p: 0 }} />
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 };
 export default ResponsiveAppBar;
