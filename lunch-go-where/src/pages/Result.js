8aa3fa55e929d626875bd10dc9cd06ba96649f81
Lau Yu Yao <lauyuyao@Laus-MacBook-Pro.local>
2022-04-12 14:48:57 +0800

    Small change on result page
    Label for average time and average price is ammended

--- lunch-go-where/src/pages/Result.js
+++ lunch-go-where/src/pages/Result.js
@@ -20,7 +20,6 @@ import PaidIcon from "@mui/icons-material/Paid";
 import apis from "../utils/apiCalls";
 import { useNavigate, useParams } from "react-router-dom";
 const Result = () => {
-<<<<<<< HEAD
     const { id } = useParams();
     const navigate = useNavigate();
     const [stall, setStall] = useState({
@@ -33,6 +32,7 @@ const Result = () => {
         calcWait: "",
         calcWouldEat: "",
         calcWouldQueue: "",
+        author: "",
     });
     const [reviewSubmitted, setReviewSubmitted] = useState(false);
     const [errMessage, setErrMessage] = useState(null);
@@ -113,7 +113,7 @@ const Result = () => {
                                     sx={{
                                         width: "20vw",
                                         minWidth: "300px",
-                                        mr: "50px",
+                                        mr: "30px",
                                         maxWidth: "500px",
                                     }}
                                 >
@@ -144,6 +144,15 @@ const Result = () => {
                                                 primary={`${stall.cuisine} food`}
                                             />
                                         </ListItem>
+                                        <ListItem disablePadding>
+                                            <ListItemIcon>
+                                                <PersonIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                sx={{ fontStyle: "italic" }}
+                                                primary={`Contributed by ${stall.author.username}`}
+                                            />
+                                        </ListItem>
                                     </List>
                                 </Box>
                                 {stall.numReviews ? (
@@ -155,7 +164,7 @@ const Result = () => {
                                                 </ListItemIcon>
                                                 <ListItemText
                                                     primary={`~$${stall.calcPrice}`}
-                                                    secondary="average price"
+                                                    secondary="average wait time"
                                                 />
                                             </ListItem>
                                             <ListItem disablePadding>
@@ -167,160 +176,6 @@ const Result = () => {
                                                     secondary="average wait time"
                                                 />
                                             </ListItem>
-=======
-	const { id } = useParams();
-	const navigate = useNavigate();
-	const [ stall, setStall ] = useState({
-		stallName: "",
-		cuisine: "",
-		location: { centerName: "" },
-		img: { url: "" },
-		submittedBy: "",
-		calcPrice: "",
-		calcWait: "",
-		calcWouldEat: "",
-		calcWouldQueue: "",
-		author: ""
-	});
-	const [ reviewSubmitted, setReviewSubmitted ] = useState(false);
-	const [ errMessage, setErrMessage ] = useState(null);
-	useEffect(
-		() => {
-			setReviewSubmitted(false);
-			apis
-				.checkAuthUser()
-				.then((res) => {
-					console.log(res);
-				})
-				.catch((err) => {
-					console.log(err.response);
-					navigate("/");
-				});
-			apis
-				.getOneStall(id)
-				.then((res) => {
-					console.log(res);
-					if (res.data.stall) {
-						setErrMessage(null);
-						setStall(res.data.stall);
-					}
-				})
-				.catch((err) => {
-					if (err.response.status === 404) {
-						setErrMessage("Stall not found. Please try again");
-					}
-				});
-		},
-		[ reviewSubmitted ]
-	);
-	return (
-		<React.Fragment>
-			<ResponsiveAppBar />
-			{errMessage ? (
-				<Typography sx={{ textAlign: "center" }}>{errMessage}</Typography>
-			) : (
-				<React.Fragment>
-					<Box
-						sx={{
-							display: "flex",
-							flexDirection: "column",
-							alignItems: "flex-start",
-							justifyContent: "flex-start",
-							px: "2vw",
-							py: "5vh",
-							width: "100vw",
-							boxSizing: "border-box"
-						}}
-					>
-						<Typography sx={{ color: "primary.dark" }} variant="h3" gutterBottom>
-							{stall.stallName}
-						</Typography>
-
-						<Box
-							sx={{
-								display: "flex",
-								flexDirection: "row",
-								justifyContent: "space-between",
-								width: "100%",
-								alignItems: "flex-start",
-								flexWrap: "wrap"
-							}}
-						>
-							<Box
-								sx={{
-									display: "flex",
-									flexDirection: "row",
-									alignItems: "flex-start",
-									flexWrap: "wrap",
-									mr: "50px"
-									// width: '60vw'
-								}}
-							>
-								<Box
-									sx={{
-										width: "20vw",
-										minWidth: "300px",
-										mr: "30px",
-										maxWidth: "500px"
-									}}
-								>
-									<img
-										src={stall.img.url}
-										alt={stall.stallName}
-										style={{ width: "100%", borderRadius: "2vw" }}
-									/>
-									<List>
-										<ListItem disablePadding>
-											<ListItemIcon>
-												<LocationOnIcon />
-											</ListItemIcon>
-											<ListItemText
-												sx={{ fontStyle: "italic" }}
-												primary={`${stall.location.centerName}`}
-											/>
-										</ListItem>
-										<ListItem disablePadding>
-											<ListItemIcon>
-												<RestaurantIcon />
-											</ListItemIcon>
-											<ListItemText
-												sx={{ fontStyle: "italic" }}
-												primary={`${stall.cuisine} food`}
-											/>
-										</ListItem>
-										<ListItem disablePadding>
-											<ListItemIcon>
-												<PersonIcon />
-											</ListItemIcon>
-											<ListItemText
-												sx={{ fontStyle: "italic" }}
-												primary={`Contributed by ${stall.author.username}`}
-											/>
-										</ListItem>
-									</List>
-								</Box>
-								{stall.numReviews ? (
-									<Box>
-										<List>
-											<ListItem disablePadding>
-												<ListItemIcon>
-													<PaidIcon />
-												</ListItemIcon>
-												<ListItemText
-													primary={`~$${stall.calcPrice}`}
-													secondary="average wait time"
-												/>
-											</ListItem>
-											<ListItem disablePadding>
-												<ListItemIcon>
-													<AccessTimeIcon />
-												</ListItemIcon>
-												<ListItemText
-													primary={`~${stall.calcWait} min`}
-													secondary="average wait time"
-												/>
-											</ListItem>
->>>>>>> master
 
                                             <ListItem disablePadding>
                                                 <ListItemIcon>
