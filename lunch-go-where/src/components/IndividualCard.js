32d96a6240c4b2185d510e65a72857634eea4591
Sarahqtw <Sarahqtw@hotmail.com>
2022-04-12 18:31:57 +0800

    styling fixes for cards

--- lunch-go-where/src/components/IndividualCard.js
+++ lunch-go-where/src/components/IndividualCard.js
@@ -1,67 +1,76 @@
 import React from "react";
 import {
-    Typography,
-    TextField,
-    FormControl,
-    InputLabel,
-    Button,
-    InputAdornment,
-    Box,
-    Divider,
-    FormLabel,
-    Modal,
-    Card,
+	Typography,
+	TextField,
+	FormControl,
+	InputLabel,
+	Button,
+	InputAdornment,
+	Box,
+	Divider,
+	FormLabel,
+	Modal,
+	Card
 } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useNavigate } from "react-router-dom";
 
 const IndividualCard = (props) => {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const handleClick = () => {
-        navigate(`/result/${props.id}`);
-    };
+	const handleClick = () => {
+		navigate(`/result/${props.id}`);
+	};
 
-    return (
-        <>
-            <Card
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    padding: "30px",
-                    minHeight: "400px",
-                    minWidth: "300px",
-                    flexWrap: "wrap",
-                    m: "30px",
-                }}
-            >
-                <img
-                    style={{ width: "300px", height: "250px" }}
-                    src={props.img}
-                    alt="Image of stall"
-                />
-                <Typography sx={{ mt: "10px" }}>
-                    Stall name: {props.nameOfStall}
-                </Typography>
-                <Typography sx={{ mt: "10px" }}>
-                    Cuisine: {props.cuisine}
-                </Typography>
-                <Typography sx={{ mt: "10px" }}>
-                    Location: {props.location}
-                </Typography>
-                <Button
-                    onClick={handleClick}
-                    endIcon={<ArrowForwardIosIcon />}
-                    variant="contained"
-                    type="submit"
-                    sx={{ width: "150px", mt: "30px" }}
-                >
-                    View Stall
-                </Button>
-            </Card>
-        </>
-    );
+	return (
+		<React.Fragment>
+			<Card
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					padding: "30px",
+					minHeight: "400px",
+					minWidth: "300px",
+					maxWidth: "400px",
+					flexWrap: "wrap",
+					m: "30px"
+				}}
+			>
+				<img
+					style={{ width: "300px", height: "250px", objectFit: "cover" }}
+					src={props.img}
+					alt={props.nameOfStall}
+				/>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "flex-start",
+						width: "100%"
+					}}
+				>
+					<Typography variant="h6" sx={{ mt: "10px" }}>
+						{props.nameOfStall}
+					</Typography>
+					<Typography sx={{ mt: "10px", fontStyle: "italic" }}>at {props.location}</Typography>
+					<Typography sx={{ mt: "10px" }}>
+						{props.cuisine} {props.cuisine === "Others" ? null : "food"}
+					</Typography>
+				</Box>
+				<Button
+					fullWidth
+					onClick={handleClick}
+					endIcon={<ArrowForwardIosIcon />}
+					variant="contained"
+					type="submit"
+					sx={{ mt: "30px" }}
+				>
+					View Stall
+				</Button>
+			</Card>
+		</React.Fragment>
+	);
 };
 
 export default IndividualCard;
